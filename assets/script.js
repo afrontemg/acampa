7202a24c017a0207dd74a953ec3c3a196cf657f4
afrontemg <166676629+afrontemg@users.noreply.github.com>
2024-04-20 14:12:53 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,33 @@
-function submitForm() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
+const formulario = document.querySelector('form');
+const confirmacoes = document.getElementById('confirmacao');
 
-  // Aqui inserimos o URL do Apps Script
-  var url = 'https://script.google.com/macros/s/AKfycbzYXLHJjn_sfMT_GcXvzAa7PgAgTcRTTbTTKQU_y2akQ4S1GnOGZNNzATd8fmsg-Ixh/exec';
-  google.script.run.withSuccessHandler(function() {
-      alert('Formulário enviado com sucesso!');
-  }).withFailureHandler(function(error) {
-      alert('Erro ao enviar formulário: ' + error.message);
-  }).processForm(name, email, url);
-
-  document.getElementById("myForm").reset();
+function enviadoSucesso() {
+    formulario.style.display = 'none';
+    confirmacoes.style.display = 'block';
 }
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('input[name=name]').value;
+    const whatsapp = document.querySelector('input[name=whatsapp]').value;
+    const cidade = document.querySelector('input[name=cidade]').value;
+    const universidade = document.querySelector('input[name=universidade]').value;
+    const curso = document.querySelector('input[name=curso]').value;
+    const afronte = document.querySelector('input[name=afronte]').value;
+
+    fetch('https://api.sheetmonkey.io/form/gmJqjW81TvgWjgQVh4Dmno', {
+    method: 'post',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, whatsapp, cidade, universidade, curso, afronte}),
+    });
+
+    enviadoSucesso();
+}
+
+
+
+formulario.addEventListener('submit', handleSubmit);
